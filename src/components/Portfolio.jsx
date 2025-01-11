917c9997ed8916aa23aee60de3fd4f69168f2e04
YuxinYuna <yuxin.shi.1019@gmail.com>
2025-01-11 02:25:14 -0800

    deploy built files

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -77,11 +77,11 @@ const Portfolio = () => {
                     <h3 className='text-2xl font-semibold text-gray-200 mb-4'>{project.title}</h3>
                     <p className='text-gray-300 mb-4'>{project.description}</p>
                     <div className='flex space-x-4'>
-                        <a href={project.links.site}
+                        {/* <a href={project.links.site}
                             className='px-4 py-2 bg-slate-600 text-gray-200 rounded-lg hover:bg-slate-700
                                         transition duration-300'>
                             View Site
-                        </a>
+                        </a> */}
                         <a href={project.links.github}
                             className='px-4 py-2 bg-slate-600 text-gray-200 rounded-lg hover:bg-slate-700
                                         transition duration-300'>
